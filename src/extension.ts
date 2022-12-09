6e787dcd6281b46ead7a7e9b8c0ddf53a639aa7d
Neil Macneale V <neil.macneale.v@gmail.com>
2022-12-09 00:17:22 -0800

    Rename things to fql, instead of fqlx

--- src/extension.ts
+++ src/extension.ts
@@ -15,18 +15,13 @@ let client: LanguageClient;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "fqlx" is now active!');
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('fqlx.helloWorld', () => {
+  let disposable = vscode.commands.registerCommand('fql.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from fqlx!');
+    vscode.window.showInformationMessage('Hello World from fql!');
   });
 
   context.subscriptions.push(disposable);
@@ -58,7 +53,7 @@ export function activate(context: vscode.ExtensionContext) {
     // Register the server for plain text documents
     documentSelector: [{
       scheme: 'file',
-      language: 'fqlx'
+      language: 'fql'
     }],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
@@ -68,8 +63,8 @@ export function activate(context: vscode.ExtensionContext) {
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    'fqlx',
-    'FQL X',
+    'fql',
+    'FQL',
     serverOptions,
     clientOptions
   );
